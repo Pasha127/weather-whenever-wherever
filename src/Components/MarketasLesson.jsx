7f329410cd10066321b4282156eac42d3ddf2b87
Paul Levitsky <pasha.account@gmail.com>
2022-09-16 16:20:24 +0200

    404 updated

---
+++ src/Components/MarketasLesson.jsx
@@ -0,0 +1,170 @@
+listOfWords1 = [absense ,
+adress ,
+alot ,
+aquit ,
+arguement ,
+artic ,
+begining ,
+beleive ,
+broccolli ,
+
+calender ,
+comraderie ,
+cieling ,
+cemetary ,
+changable ,
+collegue ,
+consciencious,
+concious ,
+daquiri,
+decieve ,
+definately,
+desparate ,
+disasterous ,
+dumbell ,
+embarass ,
+enviroment ,
+existance ,
+experiance ,
+facinating ,
+Febuary ,
+firey ,
+flourescent ,
+foriegn ,
+goverment ,
+gratefull,
+guarantee,
+harrass ,
+hieght ,
+hipocrite ,
+humerous ,
+innoculate , 
+independant ,
+jewelry ,
+
+kernel ,
+liesure ,
+liason , 
+libary ,
+lisense ,
+maintainance ,
+medevil ,
+millenium ,
+miniture ,
+miniscule ,
+mischievious ,
+mispell ,
+momento ,
+misterious ,
+neccessary ,
+nieghbor ,
+noticable , 
+occassion ,
+occurence ,
+oddyssey ,
+peice ,
+playwrite ,
+preceed ,
+presance ,
+privelege ,
+
+pumkin ,
+rasberry ,
+recieve ,
+rtyhm ,
+sacriligious ,
+sience ,
+sissors ,
+seperate ,
+sinserely ,
+supercede ,
+truely ,
+untill ,
+Wensday ,
+wether,
+wich ,
+wierd ]
+listOfWords2=[absence
+    address
+    advise, the verb, is often switched with advice, the noun
+    a lot
+    acquit
+    argument
+    arctic
+    beginning
+    believe
+    broccoli
+    orough and bureau are confused
+    calendar
+    camaraderie
+    ceiling
+    cemetery
+    changeable
+    colleague)
+    (conscientious
+    conscious
+    (daiquiri
+    deceive
+    (definitely
+    desperate
+    disastrous
+    dumbbell
+    embarrass
+    environment
+    existence
+    experience
+    fascinating
+    February
+    fiery
+    fluorescent
+    foreign
+    government
+    grateful
+    harass
+    height
+    hypocrite
+    humorous
+    inoculate
+    independent
+    jewelery
+    kernal
+    leisure
+    liaison
+    library
+    license
+    maintenance
+    (medieval
+    millennium
+    miniature
+    minuscule
+    mischievous
+    misspell
+    memento--momento is Spanish for "moment"
+    mysterious
+    necessary
+    neighbor
+    noticeable
+    occasion
+    occurrence
+    odyssey
+    piece
+    playwright
+    precede
+    presence
+    privilege
+    pumpkin
+    raspberry
+    receive
+    rhythm
+    sacrilegious
+    science
+    scissors
+    separate
+    sincerely
+    supersede
+    truly
+    until, or till
+    Wednesday
+    which, or witch)
+    whether
+    (weird)]
