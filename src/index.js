a8614ca85743ddaf1dcb3c608aa2a722bb47c0c8
Pasha127 <pasha.account@gmail.com>
2022-09-19 13:59:53 +0200

    redux adv initial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import store from "./Redux/Store";
 import { Provider } from "react-redux";
-
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
